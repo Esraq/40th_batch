9d6cc9f4fd8041fe495a4c467672d2423dee171f
Esraq <esraq26552@gmail.com>
2023-08-02 22:01:03 +0600

    First Commit

---
+++ example_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	
+	
+	
+	printf("hello world\n");
+	printf("I am from Bangladesh");
+	
+	
+	
+	
+	return 0;
+}
---
+++ example_4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	
+	
+   int a,b,add,substract,mul,div;
+   
+   printf("Enter the value of a:");
+   scanf("%d",&a);
+   printf("Enter the value of b:");
+   scanf("%d",&b);
+   
+   
+   
+   
+   add=a+b;	
+   substract=a-b;
+   mul=a*b;
+   div=a/b;
+   
+   printf("Add=%d\n",add);
+   printf("Substract=%d\n",substract);
+   printf("Multipication=%d\n",mul);
+   printf("Division=%d\n",div);
+	
+	
+	return 0;
+}
